90992688638c46a453fb9790632044cd82a5c7f4
David Dieperink <david.dieperink@ik.me>
2022-05-25 11:25:55 +0200

    Event listener

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 
 import '../css/style.css';
 import * as THREE from 'three';
+import {GUI} from 'dat.gui';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import {createEarth, createClouds, createStars} from './objects/earth.js';
 import {addCubicRoom, addRectangularRoom} from './objects/room.js';
@@ -23,6 +24,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 const controls = new OrbitControls(camera, renderer.domElement);
+var gui = new GUI();
+
 
 document.body.appendChild(renderer.domElement);
 
@@ -30,6 +33,12 @@ document.body.appendChild(renderer.domElement);
 const earth = createEarth();
 const clouds = createClouds();
 const stars = createStars();
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 // Adding these object to the scene
 scene.add(stars);
@@ -43,13 +52,28 @@ light.position.set(5,3,5);
 scene.add(light);
 
 addCubicRoom(scene, 2, 0, 0, 8);
-addRectangularRoom(scene, 5, 1, 0, 0, 0, 3);
+addRectangularRoom(scene, 2, 2, 0, 2, 0, 4);
+
+const cameraFolder = gui.addFolder('Camera');
+cameraFolder.add(camera.position, 'x', 0, 10);
+cameraFolder.add(camera.position, 'y', 0, 10);
+cameraFolder.add(camera.position, 'z', 0, 10);
+
+window.addEventListener("keydown", function(event) {
+	switch (event.code) {
+		case "KeyW":
+			camera.position.set(camera.position.x += 1, 0, 0);
+			break;
+	}
+})
+
 
 const tick = () => {
+	window.requestAnimationFrame(tick);
 	earth.rotation.y += 0.003;
 	clouds.rotation.y += 0.003;
 	renderer.render( scene, camera );
-	window.requestAnimationFrame(tick);
+	
 }
 
 tick();
